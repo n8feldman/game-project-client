96c88f775e7b71afbe967d671d81418256f61993
n8feldman <nfeldman1995@gmail.com>
2021-07-16 13:52:25 -0400

    Build sign-up curl script. Fix URLs in config

---
+++ curl-scripts/sign-up.sh
@@ -0,0 +1,16 @@
+# VARIABLE=VALUE sh curl-scripts/auth/sign-up.sh
+
+# don't use a password you use for any real websites!
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "my1@example.email",
+      "password": "my example password",
+      "password_confirmation": "my example password"
+    }
+  }'
+
+echo
