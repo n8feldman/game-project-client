b403464b69a1c81180771053aa13378248dc4fe3
n8feldman <nfeldman1995@gmail.com>
2021-07-19 10:53:48 -0400

    more touchup to gameui logic

--- app/gameui.js
+++ app/gameui.js
@@ -3,19 +3,20 @@
 const store = require('./store')
 
 const onNewGameSuccess = response => {
-  const gameID = response.game._id
-  store.gameID = gameID
-  updateBoard(gameID, response.game.cells, 'O')
+  // Is this the best syntax?
+  const gameID = response.game_id; store.gameID = gameID
+  const cells = response.game.cells; store.cells = cells
+  const over = response.game.over; store.over = over
+  const nextMove = 'X'; store.nextMove = nextMove
+  updateBoard(gameID, cells, nextMove, over)
 }
 
 const onNewGameFailure = () => {
   $('#new-game-failure').html('Failed to start game')
 }
 
-const updateBoard = (gameID, cells, lastMove) => {
+const updateBoard = (gameID, cells, nextMove, over) => {
   if ($('#game-board').is(':hidden')) $('#game-board').show()
-  let nextMove = ''
-  lastMove === 'X' ? nextMove = 'O' : nextMove = 'X'
   $('#turn-indicator').text(`Next turn: ${nextMove}`)
 }
 
